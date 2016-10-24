e6e52d88e04956a4f88a898465b5173be121718f
alani <alain.schaefer@gmail.com>
2016-10-25 05:12:08 +0900

    migrate to real KAPolyglott Server API

--- ext/src/common/es6-src/config.js
+++ ext/src/common/es6-src/config.js
@@ -1,8 +1,14 @@
 const CONFIG = {
   //supportedURLPattren : /https\:\/\/de\.khanacademy\.org\/math\/arithmetic\/addition\-subtraction\/basic_addition\/v\/.*/,
-  supportedURLPattren : /^https\:\/\/.*?\.khanacademy\.org\/.*?\/v\/.*/,
-  translateAPIURL     : 'https://www.kadeutsch.org/report/de/translate.php',
-  videoAPIURL         : 'https://www.kadeutsch.org/report/de/video.php',
+  videoURLPattern 	  : /^https\:\/\/.*?\.khanacademy\.org\/.*?\/v\/.*/,
+  translateAPIURL     : 'https://qa.kadeutsch.org/translate.json',
+  videoAPIURL         : 'https://qa.kadeutsch.org/videos.json',  
+  //translateAPIURL     : 'https://www.kadeutsch.org/report/de/translate.php',
+  //videoAPIURL         : 'https://www.kadeutsch.org/report/de/video.php',
+  
+  enabledLanguages : [ "de", "en", "ar"],
+  
+  
 };
 
 export default CONFIG;
--- ext/src/common/es6-src/helpers/contentHelper.js
+++ ext/src/common/es6-src/helpers/contentHelper.js
@@ -1,8 +1,24 @@
 import $ from '../vendor/jQuery';
+import CONFIG from '../config';
 
 export class contentHelper{
   constructor(){}
   
+  addTranslationItem(html, lang, str){
+		  html.push('<div class="kaext_translations_item">');
+		  html.push(''+
+					  "<img valign='bottom' src='"+kango.io.getResourceUrl("assets/images/flags/"+lang+".png")+"' />"+
+					'');
+					
+		  html.push('');
+		  html.push(str);
+		  html.push('');
+		  
+		  html.push('');
+		  html.push("<img class='clickable speak' data-lang='"+lang+"' src='"+kango.io.getResourceUrl("assets/images/Speaker_Icon.png")+"' />");
+		  html.push('');	
+		  html.push('</div>');		  
+  }
   /**
    * Generate HTML for the Popup-Window displaying the translations
    * data : Dictionary with Strings, Language
@@ -10,22 +26,18 @@ export class contentHelper{
   traslationHTML(data){
     var html = [];
     html.push('<div class="kaext_translations">');
-    $.each(data,function(k,i){
-      html.push('<div class="kaext_translations_item">');
-      html.push(''+
-                  "<img valign='bottom' src='"+kango.io.getResourceUrl("assets/images/flags/"+i.lang+".png")+"' />"+
-                '');
-                
-      html.push('');
-      html.push(i.value);
-      html.push('');
-      
-      html.push('');
-      html.push("<img class='clickable speak' data-lang='"+i.lang+"' src='"+kango.io.getResourceUrl("assets/images/Speaker_Icon.png")+"' />");
-      html.push('');
-      
-      html.push('</div>');
-    });
+	var self = this;
+	$.each(CONFIG.enabledLanguages,function(key,lang){
+
+		if ( data[lang] != undefined ) {
+		  self.addTranslationItem(html, lang, data[lang]);
+		}
+	});
+	
+	if (html.length == 1 ) {
+	  
+	  html.push("no translations for this string found");
+	}	
     html.push('</div>');
     return html.join("\n");
   }
@@ -35,9 +47,14 @@ export class contentHelper{
    **/
   videoIconsHTML(data){
     var html = [];
-    $.each(data,function(key, item){
-      html.push('<div class="vid_icon clickable vid_flag_'+item.lang+'" data-vid="'+item.youtube_id+'" data-lang="'+item.lang+'">'+
-                  '<img src="'+kango.io.getResourceUrl('assets/images/flags/'+item.lang+'.png')+'">'+
+	
+    $.each(CONFIG.enabledLanguages,function(key,lang){
+		var url = data[lang];
+		// the url has the format https://www.youtube.com/watch?v=6WwRwxkLuP0, extract the youtube_id
+		var youtube_id = url.split("=").pop();
+	
+		html.push('<div class="vid_icon clickable vid_flag_'+lang+'" data-vid="'+youtube_id+'" data-lang="'+lang+'">'+
+                  '<img src="'+kango.io.getResourceUrl('assets/images/flags/'+lang+'.png')+'">'+
                 '</div>');
     })
     return html.join("\n");
